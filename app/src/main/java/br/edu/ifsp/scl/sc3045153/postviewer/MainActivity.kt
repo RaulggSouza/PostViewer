6e0dd1119df395077069ff41983c74daf60327b9
RaulggSouza <raulggsouza@hotmail.com>
2026-06-18 17:32:18 -0300

    feat: connect post list to real API data

--- app/src/main/java/br/edu/ifsp/scl/sc3045153/postviewer/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3045153/postviewer/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import br.edu.ifsp.scl.sc3045153.postviewer.data.model.Post
+import br.edu.ifsp.scl.sc3045153.postviewer.ui.screens.postlist.PostListRoute
 import br.edu.ifsp.scl.sc3045153.postviewer.ui.screens.postlist.PostListScreen
 import br.edu.ifsp.scl.sc3045153.postviewer.ui.theme.PostViewerTheme
 
@@ -34,29 +35,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PostViewerApp(modifier: Modifier = Modifier) {
-    val fakePosts = listOf(
-        Post(
-            userId = 1,
-            id = 1,
-            title = "Primeiro post",
-            body = "Este é um exemplo de post sendo exibido na tela."
-        ),
-        Post(
-            userId = 1,
-            id = 2,
-            title = "Segundo post",
-            body = "Depois vamos trocar esses dados falsos pelos dados da API."
-        ),
-        Post(
-            userId = 2,
-            id = 3,
-            title = "Terceiro post",
-            body = "Cada item aparece dentro de um Card clicável."
-        )
-    )
-
-    PostListScreen(
-        posts = fakePosts,
+    PostListRoute(
         onPostClick = { post ->
             println("Post clicado: ${post.id}")
         }
--- app/src/main/java/br/edu/ifsp/scl/sc3045153/postviewer/ui/screens/postlist/PostListScreen.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3045153/postviewer/ui/screens/postlist/PostListScreen.kt
@@ -2,6 +2,7 @@ package br.edu.ifsp.scl.sc3045153.postviewer.ui.screens.postlist
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,49 +10,142 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import br.edu.ifsp.scl.sc3045153.postviewer.data.model.Post
+import br.edu.ifsp.scl.sc3045153.postviewer.ui.screens.viewmodel.PostListViewModel
+
+@Composable
+fun PostListRoute(viewModel: PostListViewModel = viewModel(), onPostClick: (Post) -> Unit) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    PostListScreen(
+        uiState = uiState,
+        onPostClick = onPostClick,
+        onRetryClick = viewModel::loadPosts
+    )
+}
 
 @Composable
 fun PostListScreen(
-    posts: List<Post>,
+    uiState: PostListUiState,
     onPostClick: (Post) -> Unit,
+    onRetryClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(modifier = modifier
         .fillMaxSize()
-        .padding(16.dp)) {
+        .padding(16.dp)
+    ) {
         Text(
             text = "PostViewer",
             style = MaterialTheme.typography.headlineMedium,
             fontWeight = FontWeight.Bold
         )
 
-        LazyColumn(
-            contentPadding = PaddingValues(top = 16.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp)
-        ) {
-            items(posts) { post ->
-                PostItem(post = post, onClick = { onPostClick(post)})
+        when {
+            uiState.isLoading -> {
+                LoadingContent()
+            }
+
+            uiState.errorMessage != null -> {
+                ErrorContent(
+                    message = uiState.errorMessage,
+                    onRetryClick = onRetryClick
+                )
+            }
+
+            else -> {
+                PostListContent(
+                    posts = uiState.posts,
+                    onPostClick = onPostClick
+                )
             }
         }
     }
 }
 
 @Composable
-private fun PostItem(post: Post, onClick: () -> Unit) {
+private fun LoadingContent() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 32.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+private fun ErrorContent(
+    message: String,
+    onRetryClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = message,
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+        Button (
+            onClick = onRetryClick,
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Text(text = "Tentar novamente")
+        }
+    }
+}
+
+@Composable
+private fun PostListContent(
+    posts: List<Post>,
+    onPostClick: (Post) -> Unit
+) {
+    LazyColumn(
+        contentPadding = PaddingValues(top = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(posts) { post ->
+            PostItem(
+                post = post,
+                onClick = { onPostClick(post) }
+            )
+        }
+    }
+}
+
+@Composable
+private fun PostItem(
+    post: Post,
+    onClick: () -> Unit
+) {
     Card(
-        modifier = Modifier.fillMaxWidth().clickable { onClick() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() },
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
-        Column(modifier = Modifier.padding(16.dp)) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
             Text(
                 text = post.title,
                 style = MaterialTheme.typography.titleMedium,
